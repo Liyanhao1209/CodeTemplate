140e28799ca88a2dd3dac14ae9f32e2998dd7799
Liyanhao1209 <3054117680@qq.com>
2023-12-13 15:25:16 +0800

    Graph:Path: Floyd

---
+++ Graph/Path/Floyd/Floyd.java
@@ -0,0 +1,30 @@
+package Graph.Path.Floyd;
+
+public class Floyd {
+    public Integer[][] shortestPath(Integer[][] g){
+        int n = g.length;
+        Integer[][] ans = g.clone(); // ought to be equal
+        Integer[][] dp;
+        for(int k=0;k<n;k++){
+            dp=ans.clone();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(i==j){
+                        continue;
+                    }
+                    if(dp[i][j]==null){
+                        if(dp[i][k]==null || dp[k][j]==null){
+                            ans[i][j] = null;
+                        }
+                        else{
+                            ans[i][j] = dp[i][k]+dp[k][j];
+                        }
+                        continue;
+                    }
+                    ans[i][j] = Math.min(dp[i][j],dp[i][k]+dp[k][j]); // with direction
+                }
+            }
+        }
+        return ans;
+    }
+}
