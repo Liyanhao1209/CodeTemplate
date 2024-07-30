caf00310379fd31a906f433fcd71bae5c590f3f4
Liyanhao1209 <3054117680@qq.com>
2024-07-30 16:14:10 +0800

    Seg-Tree
    template:SegTree.py
    LuoGuP3372 range_update 70/100
    LuoGuP3374 pt_update 70/100
    
    Signed-off-by: Liyanhao1209 <3054117680@qq.com>

---
+++ graph/tree/Seg-Tree/LuoGuP3372.py
@@ -0,0 +1,103 @@
+from typing import List
+
+# 带懒标记
+class node:
+    def __init__(self,l:int,r:int,sum:int,add:int) -> None:
+        self.l = l
+        self.r = r
+        self.sum = sum
+        self.add = add
+
+# 下标从1开始
+class SegTree:
+
+    def lc(self,p:int)->int:
+        return p<<1
+    
+    def rc(self,p:int)->int:
+        return (p<<1)|1
+
+    # 懒标记向下传递
+    def pushdown(self,curr:int):
+        if self.tr[curr].add:
+            self.tr[self.lc(curr)].sum += (self.tr[self.lc(curr)].r-self.tr[self.lc(curr)].l+1)*self.tr[curr].add
+            self.tr[self.rc(curr)].sum += (self.tr[self.rc(curr)].r-self.tr[self.rc(curr)].l+1)*self.tr[curr].add
+            self.tr[self.lc(curr)].add += self.tr[curr].add
+            self.tr[self.rc(curr)].add += self.tr[curr].add
+            self.tr[curr].add= 0
+
+    # 懒标记回溯
+    def pushup(self,curr:int):
+        self.tr[curr].sum = self.tr[self.lc(curr)].sum + self.tr[self.rc(curr)].sum
+    
+
+    def __init__(self,w:List[int]) -> None:
+        w = [0]+w
+        n = len(w)
+
+        self.tr = [node(-1,-1,-1,-1) for _ in range(4*n)]
+
+        def build(curr:int,l:int,r:int):
+            self.tr[curr] = node(l,r,w[l],0)
+            if l==r:
+                return
+            mid = (l+r)>>1
+            build(self.lc(curr),l,mid)
+            build(self.rc(curr),mid+1,r)
+            self.tr[curr].sum = self.tr[self.lc(curr)].sum + self.tr[self.rc(curr)].sum
+        
+        build(1,1,n-1)
+
+    def update_pt(self,curr:int,x:int,diff:int):
+        if self.tr[curr].l == self.tr[curr].r == x:
+            self.tr[curr].sum += diff
+            return
+        
+        mid = (self.tr[curr].l+self.tr[curr].r)>>1
+
+        if x<=mid:
+            self.update_pt(self.lc(curr),x,diff)
+        else:
+            self.update_pt(self.rc(curr),x,diff)
+        self.pushup(curr)
+
+    def update(self,curr:int,x:int,y:int,diff:int):
+        if x<=self.tr[curr].l and self.tr[curr].r<=y:
+            self.tr[curr].sum += (self.tr[curr].r-self.tr[curr].l+1)*diff
+            self.tr[curr].add += diff
+            return
+        mid = (self.tr[curr].l + self.tr[curr].r)>>1
+        
+        self.pushdown(curr)
+
+        if x<=mid:
+            self.update(self.lc(curr),x,y,diff)
+        if y>mid:
+            self.update(self.rc(curr),x,y,diff)
+
+        self.pushup(curr)
+    
+    def query(self,curr:int,x:int,y:int)->int:
+        if x<=self.tr[curr].l and self.tr[curr].r<=y:
+            return self.tr[curr].sum
+        
+        mid = (self.tr[curr].l+self.tr[curr].r)>>1
+        self.pushdown(curr)
+        res = 0
+        if x<=mid:
+            res += self.query(self.lc(curr),x,y)
+        if y>mid:
+            res += self.query(self.rc(curr),x,y)
+        return res
+
+n,m = tuple(map(int,input().split()))
+
+arr = list(map(int,input().split()))
+seg_t = SegTree(arr)
+
+for _ in range(m):
+    ops = list(map(int,input().split()))
+    if ops[0]==1:
+        seg_t.update(1,ops[1],ops[2],ops[3])
+    else:
+        print(seg_t.query(1,ops[1],ops[2]))
---
+++ prefix&&differential/LuoGuP3128.py
@@ -0,0 +1,63 @@
+N,K = tuple(map(int,input().split()))
+
+g = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    x,y = tuple(map(int,input().split()))
+    g[x].append(y)
+    g[y].append(x)
+
+import math
+pows = int(math.ceil(math.log2(N)))
+dep = [0 for _ in range(N+1)]
+fa = [[0 for _ in range(pows+1)] for _ in range(N+1)]
+
+def dfs(x:int,father:int):
+    dep[x] = dep[father]+1
+    fa[x][0] = father
+
+    for i in range(1,pows+1):
+        fa[x][i] = fa[fa[x][i-1]][i-1]
+    
+    for c in g[x]:
+        if c!=father:
+            dfs(c,x)
+
+def lca(s:int,t:int)->int:
+    if dep[s]<dep[t]:
+        s,t = t,s
+    
+    for i in range(pows,-1,-1):
+        if dep[fa[s][i]]>=dep[t]:
+            s = fa[s][i]
+    
+    if s==t:
+        return s
+
+    for i in range(pows,-1,-1):
+        if fa[s][i]!=fa[t][i]:
+            s,t = fa[s][i],fa[t][i]
+    
+    return fa[s][0]
+
+dfs(1,0)
+diff = [0 for _ in range(N+1)]
+for _ in range(K):
+    s,t = tuple(map(int,input().split()))
+    l = lca(s,t)
+    diff[s] += 1
+    diff[l] -= 1
+    diff[t] += 1
+    diff[fa[l][0]] -= 1
+
+pressure = 0
+def traverse(x:int,father:int)->int:
+    global pressure
+    res = 0
+    for c in g[x]:
+        if c!=father:
+            res += traverse(c,x)
+    res += diff[x]
+    pressure = max(pressure,res)
+    return res
+traverse(1,0)
+print(pressure)
